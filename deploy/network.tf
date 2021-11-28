636401e81836a61e0214388aafe1abb9af6f17d8
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-28 17:39:22 +0000

    added all the terraform public subnets

--- deploy/network.tf
+++ deploy/network.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.1.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = merge(
@@ -13,7 +13,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-  
+
   tags = merge(
     local.common_tags,
     tomap({
@@ -21,3 +21,127 @@ resource "aws_internet_gateway" "main" {
     })
   )
 }
+
+
+#####################################################
+# Public Subnets - Inbound/Outbound Internet Access #
+#####################################################
+
+#Availability Zone "A" Resources
+resource "aws_subnet" "public_a" {
+  cidr_block              = "10.1.10/24"
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-a"
+    })
+  )
+}
+
+resource "aws_route_table" "public_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-a"
+    })
+  )
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a.id
+}
+
+resource "aws_route" "public_internet_access_a" {
+  route_table_id         = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "public_a" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-a"
+    })
+  )
+}
+
+resource "aws_nat_gateway" "public_a" {
+  allocation_id = aws_eip.public_a.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-a"
+    })
+  )
+}
+
+#Availability Zone "B" Resources
+resource "aws_subnet" "public_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.2.0/24"
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-b"
+    })
+  )
+}
+
+resource "aws_route_table" "public_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-b"
+    })
+  )
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b.vpc_id
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id         = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_eip" "public_b" {
+  vpc = true
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-b"
+    })
+  )
+}
+
+resource "aws_nat_gateway" "public_b" {
+  allocation_id = aws_eip.public_b.id
+  subnet_id     = aws_subnet.public_b.id
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-public-b"
+    })
+  )
+}
+

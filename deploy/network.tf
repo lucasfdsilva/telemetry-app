4261b9c3a046eee55f1f9afc20be9905fea88215
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-28 19:02:53 +0000

    added vpc endpoint to dynamodb

--- deploy/network.tf
+++ deploy/network.tf
@@ -226,3 +226,28 @@ resource "aws_route" "private_b_internet_out" {
 }
 
 
+
+#############################
+# VPC Endpoint for DynamoDB #
+#############################
+
+resource "aws_vpc_endpoint" "dynamo_db" {
+  vpc_id = aws_vpc.main.id
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+  route_table_ids = [
+    aws_route_table.private_a.id,
+    aws_route_table.private_b.id
+  ]
+  service_name = "com.amazonaws.eu-west-1.dynamodb"
+
+  tags = merge(
+    local.common_tags,
+    tomap({
+      "Name" = "${local.prefix}-dynamodb"
+    })
+  )
+}
+

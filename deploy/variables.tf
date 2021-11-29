98f89be654ef9044af553df6199e2d3e65508585
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-29 22:37:45 +0000

    removed local ecr images name for dev env

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -10,7 +10,3 @@ variable "owner" {
   default = "lucas@acceltra.ie"
 }
 
-variable "ecr_image_telemetry_app" {
-  description = "ECR image for the telemetry app"
-  default     = "128363080680.dkr.ecr.eu-west-1.amazonaws.com/telemetry-app:13ca21be1a68e45ffe495ea59eea7ea6d87d3af5"
-}

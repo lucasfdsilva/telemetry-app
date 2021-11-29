72d8361dfadeac9541ac0e963baa19a2c76e39d5
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-29 01:45:07 +0000

    update image tag

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -12,5 +12,5 @@ variable "owner" {
 
 variable "ecr_image_telemetry_app" {
   description = "ECR image for the telemetry app"
-  default     = "128363080680.dkr.ecr.eu-west-1.amazonaws.com/telemetry-app:c976b987e7d44c81cc96315f1b1d7653932f3fb7"
+  default     = "128363080680.dkr.ecr.eu-west-1.amazonaws.com/telemetry-app:13ca21be1a68e45ffe495ea59eea7ea6d87d3af5"
 }

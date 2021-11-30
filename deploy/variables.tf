64e80418d330fb3ee859b25e4da20fca47fb369b
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-30 00:47:46 +0000

    added tag latest to ecr image

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -12,5 +12,5 @@ variable "owner" {
 
 variable "telemetry_app_image" {
   description = "used to store the ECR image generate in the build_push stage of the ci/cd workflow"
-  default     = "128363080680.dkr.ecr.eu-west-1.amazonaws.com/telemetry-app:b1fb29ab12b7bbf95125509a8c885e79d71f5883"
+  default     = "128363080680.dkr.ecr.eu-west-1.amazonaws.com/telemetry-app:latest"
 }

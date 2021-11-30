4b5c89d61c521a97c80e5113550e94a9f61d866f
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-30 21:51:56 +0000

    added simple html home as placeholder

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
+from resources.home import Home
 from resources.temperature import TemperatureReading
 from resources.stats import Stats
 
@@ -8,5 +9,6 @@ from resources.stats import Stats
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(TemperatureReading, '/api/temperature')
+api.add_resource(Home, '/')
 api.add_resource(Stats, '/api/stats')
+api.add_resource(TemperatureReading, '/api/temperature')

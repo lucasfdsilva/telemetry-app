4d465ee3ac1ca6dcd7511174b0ef30c93609e66d
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-30 00:25:05 +0000

    .

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -9,3 +9,8 @@ variable "project" {
 variable "owner" {
   default = "lucas@acceltra.ie"
 }
+
+variable "telemetry_app_image" {
+  description = "used to store the ECR image generate in the build_push stage of the ci/cd workflow"
+  default     = "128363080680.dkr.ecr.eu-west-1.amazonaws.com/telemetry-app:b1fb29ab12b7bbf95125509a8c885e79d71f5883"
+}

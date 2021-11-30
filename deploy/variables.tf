4138e17be6dd41556d9e88f2715fb8a57ce3b99a
Lucas Da Silva <lucas.silva00@outlook.ie>
2021-11-30 11:50:01 +0000

    added the dns resource to tf

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -14,3 +14,18 @@ variable "telemetry_app_image" {
   description = "used to store the ECR image generate in the build_push stage of the ci/cd workflow"
   default     = "128363080680.dkr.ecr.eu-west-1.amazonaws.com/telemetry-app:latest"
 }
+
+variable "dns_zone_name" {
+  description = "Domain name"
+  default     = "lucastelemetry3m.com"
+}
+
+variable "subdomain" {
+  description = "Subdomain per environment"
+  type        = map(string)
+  default = {
+    "prod"    = ""
+    "staging" = "staging."
+    "dev"     = "dev."
+  }
+}
